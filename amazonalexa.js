6b3a93c105992ae159a08ad9f30a75a6d41a974c
Marcode777 <marcosebello7@gmail.com>
2017-02-28 05:19:01 -0500

    fully functioning

--- amazonalexa.js
+++ amazonalexa.js
@@ -25,19 +25,20 @@ exports.handler = (event, context) => {
 
     case "IntentRequest":
     // > Intent Request
-      console.log("intent request!"); // endpoint added here below
+      console.log("intent request!"); 
+      console.log("come on and work!");// endpoint added here below
       switch(event.request.intent.name){
         case "getWeatherIntent":
-          var endpoint = "https://jsonplaceholder.typicode.com/users"; // this works with this "placebo endpoint data" https://jsonplaceholder.typicode.com/posts ***** api.openweathermap.org/data/2.5/weather?zip=10005,us&APPID=08d6215ef934232110949692d5ffb8da
+          var endpoint = "https://jsonplaceholder.typicode.com/posts/1"; // this works with this "placebo endpoint data" https://jsonplaceholder.typicode.com/posts ***** api.openweathermap.org/data/2.5/weather?zip=10005,us&APPID=08d6215ef934232110949692d5ffb8da
           var body = ""
           https.get(endpoint, (response) => {
             response.on('data', (chunk) => {body += chunk})
             response.on('end', () => {
               var data = JSON.parse(body);
-              var weatherCount = data.users; // might have something to do with this variable
+              var weatherCount = data.userId; // might have something to do with this variable
               context.succeed(
                 generateResponse(
-                  buildSpeechletResponse("current is ${weatherCount}", true),
+                  buildSpeechletResponse(`current is ${weatherCount}`, true), // another key was to change the quotes surrounding object literals into backticks
                   {}
                   )
                 )
